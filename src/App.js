7fefe55e972027305ed790469b576c424d6ae328
muhammad-shahroze <muhammad.shahroze@outlook.com>
2021-10-04 01:27:39 +0100

    Added Routes to App.js + added Redux Shopping folders and setup reducer & middleware + sorted App.js styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
+import React from "react";
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Products from "./Components/Products";
+import Basket from "./Components/Basket/Basket";
+import SingleItem from "./Components/SingleItem";
+import { connect } from "react-redux";
 
-function App() {
+function App({ currentItem }) {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Products} />
+          <Route path="/basket" exact component={Basket} />
+          {!currentItem ? (
+            <Redirect to="/" />
+          ) : (
+            <Route exact path="/products/:id" component={SingleItem} />
+          )}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    currentItem: state.shop.currentItem,
+  };
+};
+
+export default connect(mapStateToProps)(App);
